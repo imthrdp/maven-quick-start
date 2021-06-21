c6a3944bf4eb084b0d1104fd513510f0c0e84734
minhtruong <nmtruong96@gmail.com>
2021-06-21 21:05:41 +0700

    Add compile plugin to project

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,27 @@
 package clinic.programming.training;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Application {
-    
-    public Application() {
-        System.out.println ("Inside Application");
-    }
 
-    // method main(): ALWAYS the APPLICATION entry point
-    public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-	Application app = new Application();
-    }
+	public Application() {
+		System.out.println("Inside Application");
+	}
+
+	public void greet() {
+		List<String> greetings = new ArrayList<String>();
+
+		greetings.add("Hi there!");
+
+		for (String greeting : greetings) {
+			System.out.println("Greeting: " + greeting);
+		}
+	}
+
+	// method main(): ALWAYS the APPLICATION entry point
+	public static void main(String[] args) {
+		System.out.println("Starting Application");
+		Application app = new Application();
+		app.greet();
+	}
 }
